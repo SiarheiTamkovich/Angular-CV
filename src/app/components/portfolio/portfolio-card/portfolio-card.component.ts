b45fcae80a23f0d63adf5260cd275214d441c8f1
SiarheiTamkovich <mpg2000@mail.ru>
2022-10-19 02:23:00 +0300

    feat: add animation to project cards

--- src/app/components/portfolio/portfolio-card/portfolio-card.component.ts
+++ src/app/components/portfolio/portfolio-card/portfolio-card.component.ts
@@ -1,15 +1,58 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input } from '@angular/core';
 import { ProjectModel } from '../project.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+declare const VanillaTilt: { init: (arg0: NodeListOf<Element>, arg1: { max: number; speed: number; }) => void; };
+
+
+const styleOn = style({
+  opacity: 1,
+})
+
+const styleOff = style({
+  opacity: 0,
+})
 
 @Component({
   selector: 'app-portfolio-card',
   templateUrl: './portfolio-card.component.html',
-  styleUrls: ['./portfolio-card.component.scss']
+  styleUrls: ['./portfolio-card.component.scss'],
+  animations: [
+    trigger('opacityAnimationUp', [
+      state('show', styleOn),
+      state('hide', styleOff),
+      transition('show => hide', animate('900ms ease-out')),
+      transition('hide => show', animate('900ms ease-in'))
+    ]),
+  ],
 })
-export class PortfolioCardComponent {
+export class PortfolioCardComponent implements AfterViewInit {
 
   @Input() public projects!: ProjectModel[];
 
-  constructor() { }
+  state = 'hide'
+
+  constructor(public element: ElementRef) { }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.element.nativeElement.offsetTop
+    const scrollPosition = window.pageYOffset
+    if (scrollPosition >= componentPosition - 400) {
+      this.state = 'show'
+    } else {
+      this.state = 'hide'
+    }
+  }
+
+  ngAfterViewInit(): void {
+
+    const cards = document.querySelectorAll(".project-card");
+
+    VanillaTilt.init(cards, {
+      max: 10,
+      speed: 500,
+    });
+  }
 
 }
