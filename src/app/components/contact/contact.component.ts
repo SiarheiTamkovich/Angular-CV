efe3f27400621e9610710ba2434e3e91a4306fe1
SiarheiTamkovich <mpg2000@mail.ru>
2022-11-06 13:50:00 +0300

    feat: add styles to form field

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { ContactService } from '../../services/contact.service';
 
@@ -8,13 +8,20 @@ import { ContactService } from '../../services/contact.service';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  FormData!: FormGroup;
+  form!: FormGroup;
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  name: FormControl = new FormControl('', [Validators.required]);
+  message: FormControl = new FormControl('', [Validators.required, Validators.maxLength(256)]);
 
-  constructor(private builder: FormBuilder, private contact: ContactService) { }
-
-  email = new FormControl('', [Validators.required, Validators.email]);
+  constructor(private builder: FormBuilder, private contact: ContactService) { 
+    this.form = this.builder.group({
+      email: this.email,
+      name: this.name,
+      message: this.message,
+    });
+  }
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
@@ -23,26 +30,16 @@ export class ContactComponent implements OnInit {
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
-  ngOnInit() {
-    this.FormData = this.builder.group({
-      Fullname: new FormControl('', [Validators.required]),
-      Email: new FormControl('', [Validators.required, Validators.email]),
-      Comment: new FormControl('', [Validators.required])
-    });
-    console.log(this.FormData)
-  }
-
-
-  onSubmit(FormData: any) {
-    console.log(FormData)
-    this.contact.PostMessage(FormData)
-      .subscribe(response => {
-        location.href = 'https://mailthis.to/confirm'
-        console.log(response)
-      }, error => {
-        console.warn(error.responseText)
-        console.log({ error })
-      })
+  onSubmit(form: any) {
+    console.log(form);
+    // this.contact.PostMessage(form)
+    //   .subscribe(response => {
+    //     location.href = 'https://mailthis.to/confirm'
+    //     console.log(response)
+    //   }, error => {
+    //     console.warn(error.responseText)
+    //     console.log({ error })
+    //   })
   }
 
 
